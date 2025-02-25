b8957500095e73556697e95fcad4f506aee24ad1
Coner McCoy <200731565+ctmccoy@users.noreply.github.com>
2025-02-25 04:03:57 -0500

    Delete Kilometers.py

--- Kilometers.py
+++
@@ -1,7 +0,0 @@
-# kilometers.py
-#the following program takes miles as input and converts to kilometers as output
-
-def convertToKilometers(miles):
-    kilometers = miles / 0.621371
-    return kilometers
-
